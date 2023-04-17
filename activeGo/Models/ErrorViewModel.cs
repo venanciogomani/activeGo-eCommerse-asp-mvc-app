c98f60be21bdfd2d4d2f665b6375a40ea7bf656b
venancio_gomani <venancio.gomani@mrisoftware.com>
2023-04-17 22:44:28 +0200

    Add project files.

---
+++ activeGo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace activeGo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
